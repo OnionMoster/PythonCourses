d7e06f2100548d7e66904bafc045435996795069
OnionMoster <1437748011@qq.com>
2024-09-23 23:51:48 +1000

    week2.F3

--- Week2/1.py
+++ Week2/1.py
@@ -1,50 +1,52 @@
-def apply_pattern_to_list(L, pattern='+-', from_start=True):
+def apply_pattern_to_list(L, pattern="+-", from_start=True):
     pattern_list = list(pattern)
     p = len(pattern_list)
     pop_line = {}
-    z = 0
-    if from_start: 
-        #from_start = True left to right
-        n = 0 
+    x = 0
+
+    if from_start:
+        # from_start = True left to right
+        n = 0
         while n < len(L) - 1:
             a = pattern_list[n % p]
-            if a == '+':
-                if L[n] < L[n+1]:
+            if a == "+":
+                if L[n] < L[n + 1]:
                     n += 1
                 else:
-                    z += 1
-                    pop_line[n+z] = L[n+1]
-                    L.pop(n+1)
-            elif a == '-':
-                if L[n] > L[n+1]:
+                    x += 1
+                    pop_line[n + x] = L[n + 1]
+                    L.pop(n + 1)
+            elif a == "-":
+                if L[n] > L[n + 1]:
                     n += 1
                 else:
-                    z +=1
-                    pop_line[n+z] = L[n+1]
-                    L.pop(n+1)
-    elif from_start == False:
-        #from_start = false right to left
+                    x += 1
+                    pop_line[n + x] = L[n + 1]
+                    L.pop(n + 1)
+    else:
+        # from_start = false right to left
         n = -1
-        while n > - len(L) :
+        while n > -len(L):
             b = pattern_list[n % p]
-            if b == '+':
-                if L[n] < L[n-1]:
+            if b == "+":
+                if L[n] < L[n - 1]:
                     n -= 1
                 else:
-                    z += 1
-                    pop_line[n-z] = L[n-1]
-                    L.pop(n-1)
-            elif b == '-':
-                if L[n] > L[n-1]:
+                    x += 1
+                    pop_line[n - x] = L[n - 1]
+                    L.pop(n - 1)
+            elif b == "-":
+                if L[n] > L[n - 1]:
                     n -= 1
                 else:
-                    z += 1
-                    pop_line[n-z] = L[n-1]
-                    L.pop(n-1)
+                    x += 1
+                    pop_line[n - x] = L[n - 1]
+                    L.pop(n - 1)
 
-    return pop_line, L
+    return pop_line
 
 
 L = [0, -4, 4, 4, 2, -2, 1, 3, -3, -4, -4, -2, -3, 0, 1, 2, -4, 3, -1, 1]
-z =  apply_pattern_to_list(L, '-+', False)
-print(z)
+x = apply_pattern_to_list(L, "-+")
+print(x)
+print(L)
