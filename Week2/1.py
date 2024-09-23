46e3111e0f66c7e202cdd6a5ef6916d42efc0c7e
OnionMoster <1437748011@qq.com>
2024-09-23 18:18:53 +1000

    week2.2

--- Week2/1.py
+++ Week2/1.py
@@ -13,18 +13,20 @@ def apply_pattern_to_list(L, pattern='+-', from_start=True):
                     n += 1
                 else:
                     z += 1
-                    pop_line[n+z+1] = L[n+1]
+                    pop_line[n+z] = L[n+1]
                     L.pop(n+1)
             elif a == '-':
                 if L[n] > L[n+1]:
                     n += 1
                 else:
                     z +=1
-                    pop_line[n+1+1] = L[n+1]
+                    pop_line[n+1] = L[n+1]
                     L.pop(n+1)
 
     return pop_line , L
 
-L = [0, -4, 4, 4, 2, -2, 1, 3, -3, -4, -4, -2, -3, 0, 1, 2, -4, 3, -1, 1]
-rusult =  apply_pattern_to_list(L, '-+')
+L = [1, 3, 2, 0, 0, -2, -2, 1, 5, -4]
+
+rusult = apply_pattern_to_list(L, '++-')
+
 print (rusult)
