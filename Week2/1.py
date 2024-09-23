1e08b57d7b68caaa8af492dd8ef99220a7db343d
OnionMoster <1437748011@qq.com>
2024-09-23 22:45:21 +1000

    week2.F1

--- Week2/1.py
+++ Week2/1.py
@@ -2,13 +2,13 @@ def apply_pattern_to_list(L, pattern='+-', from_start=True):
     pattern_list = list(pattern)
     p = len(pattern_list)
     pop_line = {}
+    z = 0
     if from_start: 
         #from_start = True left to right
-        n = 0 #left to right
-        z = 0
+        n = 0 
         while n < len(L) - 1:
             a = pattern_list[n % p]
-            if a == '+' :
+            if a == '+':
                 if L[n] < L[n+1]:
                     n += 1
                 else:
@@ -22,10 +22,29 @@ def apply_pattern_to_list(L, pattern='+-', from_start=True):
                     z +=1
                     pop_line[n+z] = L[n+1]
                     L.pop(n+1)
+    elif from_start == False:
+        #from_start = false right to left
+        n = -1
+        while n > - len(L) :
+            b = pattern_list[n % p]
+            if b == '+':
+                if L[n] < L[n-1]:
+                    n -= 1
+                else:
+                    z += 1
+                    pop_line[n-z] = L[n-1]
+                    L.pop(n-1)
+            elif b == '-':
+                if L[n] > L[n-1]:
+                    n -= 1
+                else:
+                    z += 1
+                    pop_line[n-z] = L[n-1]
+                    L.pop(n-1)
 
-    return pop_line
+    return pop_line, L
 
 
-L = [1, 3, 2, 0, 0, -2, -2, 1, 5, -4]
-z =  apply_pattern_to_list(L, '++-')
+L = [0, -4, 4, 4, 2, -2, 1, 3, -3, -4, -4, -2, -3, 0, 1, 2, -4, 3, -1, 1]
+z =  apply_pattern_to_list(L, '-+', False)
 print(z)
