e6b328c6ff6e1af780cbac06e52c5d5ee22dd2c2
OnionMoster <1437748011@qq.com>
2024-09-26 21:26:23 +1000

    week2.F8

---
+++ Week3/1.py
@@ -0,0 +1,5 @@
+import unicodedata
+
+input_string = input(
+    "Please input two integers a and b with 0 <= a <= b <= 1114111,\n\t both integers being separated by ~, with possibly \n\t spaces and tabs before and after the numbers:"
+)
