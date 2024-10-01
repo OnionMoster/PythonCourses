1a6a6790dced4b9d6e6b72565cedf2276a958424
OnionMoster <1437748011@qq.com>
2024-10-02 00:01:05 +1000

    week3.F9

--- Week3/1.py
+++ Week3/1.py
@@ -5,13 +5,14 @@ def input_number():
     First_message = "Please input two integers a and b with 0 <= a <= b <= 1114111,\n       both integers being separated by ~, with possibly\n       spaces and tabs before and after the numbers:\n       "
     Error_message = "\nIncorrect input, try again!"
     second_message = "\nEnter a string: "
+    all_list = []
     match_list = []
 
     # 获取输入
     number = input(First_message)
     # print(number)
     # 整理输入
-    cleaned_num = number.replace(" ", "").replace("\t", "")
+    cleaned_num = number.replace(" ", "").replace("\t", "").replace("+", "")
     # print(cleaned_num)
     # 判断输入
     if cleaned_num.count("~") != 1:
@@ -22,57 +23,59 @@ def input_number():
         a, b = int(cleaned_num[0]), int(cleaned_num[1])
         # print(a, b)
         # 判断ab
-        if 0 <= a <= 1114111 and 0 <= b <= 1114111 and a <= b:
-            num_list = [i for i in range(a, b + 1)]
+        if 0 <= a <= b <= 1114111:
             x = 0
-            for i in num_list:
+            for i in range(a, b + 1):
                 try:
                     unicodedata.name(chr(i))
                     x += 1
+                    name = unicodedata.name(chr(i))
+                    all_list.append((name, chr(i)))
                 except ValueError:
                     pass
-            n = x / (b - a + 1)
+            n = (x / (b - a + 1)) * 100
             n1 = f"{(x / (b - a + 1)) * 100:.2f}"
             # print(n1)
             if n == 100:
                 if a == b:
-                    print(f"{a} is the code point of a named character.")
+                    print(f"\n{a} is the code point of a named character.")
                 else:
                     print(
-                        f"All numbers between {a} and {b} \n  are code points of named characters."
+                        f"\nAll numbers between {a} and {b}\n  are code points of named characters."
                     )
             elif 0 < n < 100:
                 print(
-                    f"Amongst the numbers between {a} and {b},\n  {n1}% are code points of named characters."
+                    f"\nAmongst the numbers between {a} and {b},\n  {n1}% are code points of named characters."
                 )
             else:
-                print(f"{a} is not the code point of a named character.")
+                if a == b:
+                    print(f"\n{a} is not the code point of a named character.")
+                else:
+                    print(
+                        f"\nNo number between {a} and {b}\n  is the code point of a named character."
+                    )
                 return ()
 
             # 匹配第二次输入
             String = input(second_message)
             # print(String)
 
+            if not all_list:
+                print(
+                    f"\nNone of the characters you want me to consider\n  has a name that starts with {String}."
+                )
+                return ()
+            else:
+                print(
+                    f"\nHere are those of the characters under consideration\n  whose name starts with {String}:"
+                )
+                max_length = max(len(name) for name, _ in all_list)
+                all_list.sort(key=lambda x: x[0])
+                for name, unicode in all_list:
+                    print(f"{name.ljust(max_length)}: {unicode}")
         else:
             print(Error_message)
-            for i in range(a, b + 1):
-                try:
-                    name = unicodedata.name(chr(i))
-                    if name.startswith(String):
-                        match_list.append((chr(i)), name)
-                except ValueError:
-                    continue
-        if not match_list:
-            print(
-                f"None of the characters you want me to consider\n  has a name that starts with {String}"
-            )
-            return ()
-        else:
-            for unicode, name in match_list:
-                print(
-                    f"Here are those of the characters under consideration\n  whose name starts with {String}:"
-                )
-                print(f"{name}:{unicode}")
+            return input_number()
 
 
 input_number()
