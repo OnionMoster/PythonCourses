701cd8388d2c3e7de9f1b541df28b75d8a955b01
OnionMoster <1437748011@qq.com>
2024-10-01 11:51:15 +1000

    week3.F0

--- Week3/1.py
+++ Week3/1.py
@@ -1,9 +1,10 @@
 import unicodedata
 
+
 def input_unicode():
     First_message = "Please input two integers a and b with 0 <= a <= b <= 1114111,\n\t both integers being separated by ~, with possibly \n\t spaces and tabs before and after the numbers:"
     Error_message = "Incorrect input, try again!"
-    second_message = 'Enter a string:'
+    second_message = "Enter a string:"
     match_list = []
 
     number = input(First_message)
@@ -15,28 +16,43 @@ def input_unicode():
         cleaned_num = cleaned_num.strip("~")
         a, b = int(cleaned_num[0]), int(cleaned_num[1])
         if 0 <= a <= 1114111 and 0 <= b <= 1114111 and a <= b:
-            # 百分比
+            # 百分比 （b-a+1）/unicode
             n = 1
             if n == 100:
                 if a == b:
-                    print(a + " is the code point of a named character.")
+                    print("{a} is the code point of a named character.")
                 else:
-                    print("All numbers between {a} and {b} \n  are code points of named characters.")
+                    print(
+                        "All numbers between {a} and {b} \n  are code points of named characters."
+                    )
             elif 0 < n < 100:
-                print("Amongst the numbers between {a} and {b},\n  8{n}% are code points of named characters.")
+                print(
+                    "Amongst the numbers between {a} and {b},\n  8{n}% are code points of named characters."
+                )
             else:
                 # print(int(a) + " is not the code point of a named character.")
                 exit
             # match unicode name
             String = input(second_message)
-            for codepoint in range(a, b):
+            for codepoint in range(a, b + 1):
                 try:
                     char_name = unicodedata.name(chr(codepoint))
                     if char_name.startswith(String):
-                        match_list.append(char_name,(chr(codepoint)))
+                        match_list.append((chr(codepoint)), char_name)
                 except ValueError:
                     continue
-            
+
+            if not match_list:
+                print(
+                    "None of the characters you want me to consider\n  has a name that starts with {String}."
+                )
+            else:
+                for point, name in match_list:
+                    print(
+                        "Here are those of the characters under consideration\n  whose name starts with {String}:"
+                    )
+                    print("{name}:{point}")
 
         else:
             print(Error_message)
+            return input_unicode()
