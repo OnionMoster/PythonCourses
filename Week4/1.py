74e5e22f588142424812d023a778417d26d24416
OnionMoster <1437748011@qq.com>
2024-10-05 19:35:18 +1000

    week4.F2

--- Week4/1.py
+++ Week4/1.py
@@ -13,9 +13,27 @@ def sieve_of_primes_up_to(n):
 def tri_numbers(n):
     bool_num = sieve_of_primes_up_to(n)
     num = [i for i in range(2, n + 1) if bool_num[i]]
+    num_list = []
+    num_dict = {}
     # print(f"{n}:{num}")
 
     # 所有三个数乘小于和等于n的
+    for a in range(len(num)):
+        for b in range(a, len(num)):
+            for c in range(b, len(num)):
+                final_num = num[a] * num[b] * num[c]
+                if final_num <= n:
+                    num_list.append(final_num)
+                    num_dict.update({final_num: (num[a], num[b], num[c])})
+
+    fianl_list = sorted(num_list)
+    fianl_dict = dict(sorted(num_dict.items()))
+    # print(fianl_list)
+    a, b, c = fianl_dict[fianl_list[-1]]
+
+    # print(f"There are {len(fianl_list)} trinumbers at most equal to {n}.")
+    # print(fianl_dict)
+    # print(f"The largest one is {fianl_list[-1]}, equal to {a} x {b} x {c}.")
 
 
 tri_numbers(123)
